7cc43ae0a992df8491bd4a0e125884d834a35b47
Kasperi Pruukka <kasperi.pruukka@gmail.com>
2022-02-15 18:50:13 +0200

    Lisätään nodeMovies kansio.
    - Alustetaan Node.js äppi.
    - Asennetaan express

--- helloexpress/index.js
+++ helloexpress/index.js
@@ -4,18 +4,32 @@ const app = express();
 
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello Express!");
-});
-
-// app.post("/hello", (req, res) => {
-//     res.send("Hello World!");
-// });
-
-app.all("/hello", (req, res) => {
-    res.send("Hello World!");
-});
-
 app.listen(port, () => {
   console.log(`Server is running on port ${port}.`);
+});
+
+app.get("/home/user/:firstname/:lastname", (req, res) => {
+    res.send(`Welcome ${req.params.firstname} ${req.params.lastname}`);
+});
+
+app.get("/home/fail/", (req, res) => {
+    // Voit lähettää jsonia.
+    //res.json({username: 'Kasperi'});
+    
+    // Send empty response with the status 404 using the status() and sendStatus() methods
+
+    // Status() -menetelmä asettaa vain tilan
+    //res.status(404).end();
+
+    //sendStatus() asettaa tilan ja lähettää vastauksen
+    res.sendStatus(404);
+});
+
+app.get("/home/json/", (req, res) => {
+    // Voit lähettää jsonia.
+    res.json({username: 'Kasperi'});
+});
+
+app.get("/about", (req, res) => {
+    res.send("About us...");
 });
