adce7d8666ac7fc0a883e19bcd27739667e5f50f
Kasperi Pruukka <kasperi.pruukka@gmail.com>
2022-02-22 19:09:01 +0200

    Muutoksia customer index.js tiedostoon.

--- nodecustomerdb/index.js
+++ nodecustomerdb/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const customerQuery = require('./db/customers');
+const query = require('./db/customers');
 
 const app = express();
 app.use(bodyParser.json());
@@ -8,20 +8,20 @@ app.use(bodyParser.json());
 const port = 3000;
 
 // Hae kaikki asiakkaat
-app.get("/api/customers", customerQuery.getAllCustomers);
+app.get("/api/customers", query.getAllCustomers);
 
 // Hae asiakas tunnuksella. Tunnus lähetetään reittiparametrissa Id
-app.get("/api/customers/:id", customerQuery.getCustomerById);
+app.get("/api/customers/:id", query.getCustomerById);
 
 // Lisää uusi asiakas - Uusi asiakas lähetetään pyynnön sisällä.
-app.post("/api/customers", customerQuery.addCustomer);
+app.post("/api/customers", query.addCustomer);
 
 // 	Poista asiakas tunnuksella. Tunnus lähetetään reittiparametrissa Id
-app.delete("/api/customers/:id", customerQuery.deleteCustomer);
+app.delete("/api/customers/:id", query.deleteCustomer);
 
 // Muokkaa asiakasta tunnuksella. 
 //Tunnus lähetetään reittiparametrina Id ja päivitettävä asiakas pyynnön sisällä.
-app.put("/api/customers/:id", customerQuery.updateCustomer);
+app.put("/api/customers/:id", query.updateCustomer);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}.`);
